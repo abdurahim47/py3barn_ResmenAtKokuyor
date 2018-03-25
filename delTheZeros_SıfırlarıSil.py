3e261664ba026d1976fbd96f76477cb9176436d3
Abdurrahim <abdurahim.abdo.zer@gmail.com>
2018-03-25 20:53:54 +0300

    Create delTheZeros_SıfırlarıSil.py

---
+++ delTheZeros_SıfırlarıSil.py
@@ -0,0 +1,19 @@
+sampleStr_ornekKatar = """
+0000000000
+0000000000
+0000000000
+0008742000
+000hgfr000
+0000000000
+0000000000
+0000000000
+"""
+
+sampleStr_ornekKatar = sampleStr_ornekKatar.replace("0","")#Change all of zeros with none, sıfırları sil
+
+print(sampleStr_ornekKatar)
+
+#The Result (without hash char), hashtag karakteri olmadan print(sampleStr_ornekKatar) ifadesinin sonucuna eşittir
+
+#8742
+#hgf
